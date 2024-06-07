61526f377d235de54e4210b3d17eb130d3d0092e
Anastasiya Shalya <a.shalja@cft.ru>
2024-06-07 16:37:40 +0700

    add db and allure

--- src/test/java/autotests/payloads/Duck.java
+++ src/test/java/autotests/payloads/Duck.java
@@ -1,6 +1,6 @@
 package autotests.payloads;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
